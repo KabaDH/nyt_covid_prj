c968f1070f40c12ea5f74921729c7a301ae1b5c2
inlevkov <io.levk@gmail.com>
2022-04-12 16:39:45 +0800

    Orientation fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:nyt_covid_prj/screens/rootscreen.dart';
 import 'package:nyt_covid_prj/services/palette.dart';
 import 'screens/screens.dart';
 
 void main() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+      .then((_) => runApp(MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Covid-19 live',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: Palette.primaryColor,
-        visualDensity: VisualDensity.adaptivePlatformDensity
-      ),
+          primarySwatch: Colors.blue,
+          scaffoldBackgroundColor: Palette.primaryColor,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
       home: RootScreen(),
     );
   }
